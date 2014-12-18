bcdb875e703ba90c5df978632c23bcedc7200c99
Jiahao Li <isundaylee.reg@gmail.com>
2014-12-18 01:59:02 -0500

    Settings file organization.

--- greekpick/settings.py
+++ greekpick/settings.py
@@ -28,7 +28,6 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -124,17 +123,3 @@ LETTER_LIST = [
   ('psi', ('Ψ', 'ψ')),
   ('omega', ('Ω', 'ω'))
 ]
-
-# # Heroku
-# # Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
-
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# # Allow all host headers
-# ALLOWED_HOSTS = ['*']
-
-# # Static asset configuration
-# STATIC_ROOT = 'staticfiles'
