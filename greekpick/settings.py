383622047b199bde5916e95a69672df59d872ace
Jiahao Li <isundaylee.reg@gmail.com>
2014-12-18 01:53:16 -0500

    Hell lotta file movings.

--- greekpick/settings.py
+++ greekpick/settings.py
@@ -93,8 +93,8 @@ STATICFILES_DIRS = (
 )
 
 # Template directories
-TEMPLATE_DIRS = (
-  'templates',
+TEMPLATE_LOADERS = (
+    'django.template.loaders.app_directories.Loader',
 )
 
 # The lovely letters :)
@@ -125,16 +125,16 @@ LETTER_LIST = [
   ('omega', ('Ω', 'ω'))
 ]
 
-# Heroku
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# # Heroku
+# # Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+# # Allow all host headers
+# ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-STATIC_ROOT = 'staticfiles'
+# # Static asset configuration
+# STATIC_ROOT = 'staticfiles'
